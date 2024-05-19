abb796182ccd7c48fc3cd6c075efaa432873604b
sarvin nami <srvn0nm@gmail.com>
2024-05-19 09:29:27 +0330

    debug imdb api

--- welcome-to-docker/phase1/imdb.py
+++ welcome-to-docker/phase1/imdb.py
@@ -1,13 +1,15 @@
 import requests
 
 def imdb(query):
-    url = f"https://imdb-movies-web-series-etc-search.p.rapidapi.com/{query}.json"
+    url = "https://imdb146.p.rapidapi.com/v1/find/"
+
+    querystring = {"query":query}
 
     headers = {
 	"X-RapidAPI-Key": "4f572370c6mshea36af5374dcaa6p10e8ddjsn40a8eb8b2bf2",
-	"X-RapidAPI-Host": "imdb-movies-web-series-etc-search.p.rapidapi.com"
+	"X-RapidAPI-Host": "imdb146.p.rapidapi.com"
 }
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, params=querystring)
     print(response.json())
     return response.json()
